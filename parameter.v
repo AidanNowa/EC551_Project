676b58b3083dcf6eaa39c7b3325100e76592370f
AidanNowa <98485635+AidanNowa@users.noreply.github.com>
2023-12-01 15:30:01 -0500

    Parameter file to select operations

---
+++ parameter.v
@@ -0,0 +1,9 @@
+
+`define INPUTFILENAME		 "/home/aidannow/EC551/Project/kodim23.hex/kodim23.hex" // Input file name
+`define OUTPUTFILENAME		 "/home/aidannow/EC551/Project/kodim23.hex/output.bmp"		// Output file name
+
+// Choose the operation of code by delete // in the beginning of the selected line
+
+//`define BRIGHTNESS_OPERATION
+`define INVERT_OPERATION
+//`define THRESHOLD_OPERATION 
