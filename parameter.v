1731f7431a11b99de1a91e1cbfa6f91fa570460b
AidanNowa <98485635+AidanNowa@users.noreply.github.com>
2023-12-07 00:16:43 -0500

    added functions, plenty of bugs

--- parameter.v
+++ parameter.v
@@ -1,9 +1,18 @@
 
 `define INPUTFILENAME		 "/home/aidannow/EC551/Project/kodim23.hex/kodim23.hex" // Input file name
-`define OUTPUTFILENAME		 "/home/aidannow/EC551/Project/kodim23.hex/output.bmp"		// Output file name
+`define OUTPUTFILENAME		 "/home/aidannow/EC551/Project/output_test_12_1.bmp"		// Output file name
 
 // Choose the operation of code by delete // in the beginning of the selected line
 
-//`define BRIGHTNESS_OPERATION
-`define INVERT_OPERATION
+//`define BRIGHTNESS_SUBTRACTION_OPERATION
+//`define BRIGHTNESS_ADDITION_OPERATION
+//`define INVERT_OPERATION
 //`define THRESHOLD_OPERATION 
+//`define GRAYSCALE_OPERATION
+//`define RED_OPERATION
+//`define GREEN_OPERATION
+//`define BLUE_OPERATION
+//`define TRIP_OPERATION
+//`define BLUR_OPERATION
+//`define SHARPEN_OPERATION
+`define OIL_OPERATION
